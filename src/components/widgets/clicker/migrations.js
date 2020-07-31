a942cfc2b34ef7cd37107bfdeccdd4073a4dc250
Sergey Buntsevich <scarry92@yandex.ru>
2020-07-31 16:44:41 +0300

    v.1.10.7[feature] multipayload for button

--- src/components/widgets/clicker/migrations.js
+++ src/components/widgets/clicker/migrations.js
@@ -21,5 +21,12 @@ export default {
       ]
     }
     return widget
+  },
+  '1.10.7': (widget) => {
+    const settings = widget.settings
+    if (typeof settings.payload === 'string') {
+      settings.topics.forEach(topic => { topic.payload = settings.payload })
+    }
+    return widget
   }
 }
